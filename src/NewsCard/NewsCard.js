e2484869004f360c4ca47e1039ad16e28531628c
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-02-26 17:55:30 +0530

    filter

--- src/NewsCard/NewsCard.js
+++ src/NewsCard/NewsCard.js
@@ -1,43 +1,45 @@
-import React, { useEffect, useState } from 'react'
-import style from "./NewsCard.module.css"
-import { getNews } from '../Api/NewsApi'
-import { searchData } from '../Recoil/Recoil'
-import { useRecoilState } from 'recoil'
+import React, { useEffect, useState } from "react";
+import style from "./NewsCard.module.css";
+import { getNews } from "../Api/NewsApi";
+import { searchData } from "../Recoil/Recoil";
+import { useRecoilState } from "recoil";
 
 function NewsCard() {
-  const [input , setInput] = useState("")
-  const [news,setNews] = useRecoilState(searchData)
+  const [input, setInput] = useState("");
+  const [news, setNews] = useRecoilState(searchData);
 
+  useEffect(() => {
+    getNews()
+      .then((data) => setNews(data.articles))
+      .catch();
+  }, []);
+  console.log(news, "sachin news");
 
-     useEffect(()=>{
-        getNews().then((data)=> setNews(data.articles)
-          ).catch()
-      },[])
-      console.log(news,"sachin news")
-     
   return (
     <>
-    <input className={style.searchbox} onChange={(e)=>setInput(e.target.value)} value={input}  placeholder="Search"/>
+      <input
+        className={style.searchbox}
+        onChange={(e) => setInput(e.target.value)}
+        value={input}
+        placeholder="Search"
+      />
 
-<div className={style.main}>
-
-  
-    {news.filter((item)=>{
-
-      return  item.title.toLowerCase().includes(input.toLowerCase())
-
-    })
-           .map((item,index)=>
-         <div key={index} className={style.container}>
-            <h6>{item?.author}</h6>
-            <img className={style.img} src={item.urlToImage} />
-            <h5>{item.title}</h5>
-             <h6>{item.publishedAt}</h6>
-          </div>
-    )}
-    </div>
+      <div className={style.main}>
+        {news
+          .filter((item) => {
+            return item.title.toLowerCase().includes(input.toLowerCase());
+          })
+          .map((item, index) => (
+            <div key={index} className={style.container}>
+              <h6>{item?.author}</h6>
+              <img className={style.img} src={item.urlToImage} />
+              <h5>{item.title}</h5>
+              <h6>{item.publishedAt}</h6>
+            </div>
+          ))}
+      </div>
     </>
-  )
+  );
 }
 
-export default NewsCard
+export default NewsCard;
