17013ce87a66bdda17ce0cf44d50e9904fc9cd55
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-02-26 23:13:01 +0530

    news

--- src/Api/NewsApi.js
+++
@@ -1,8 +0,0 @@
-
-export const getNews=()=>{
-
-    const url=`https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=b3e12763ea9f45dd8c27b3725230061f`
-    return fetch(url)
-    .then((response)=>response.json())
-    .catch((error)=>console.log("Error :" , error))
-}
--- src/NewsCard/NewsCard.js
+++ src/NewsCard/NewsCard.js
@@ -1,19 +1,30 @@
 import React, { useEffect, useState } from "react";
 import style from "./NewsCard.module.css";
-import { getNews } from "../Api/NewsApi";
 import { searchData } from "../Recoil/Recoil";
 import { useRecoilState } from "recoil";
 
 function NewsCard() {
   const [input, setInput] = useState("");
   const [news, setNews] = useRecoilState(searchData);
+ 
+
 
   useEffect(() => {
-    getNews()
-      .then((data) => setNews(data.articles))
-      .catch();
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '5854509ae6msh4cf696e59631ad3p1669e1jsn997f958fe26d',
+        'X-RapidAPI-Host': 'contextualwebsearch-websearch-v1.p.rapidapi.com'
+      }
+    };
+    
+    fetch('https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/search/NewsSearchAPI?q=taylor%20swift&pageNumber=1&pageSize=10&autoCorrect=true&fromPublishedDate=null&toPublishedDate=null', options)
+      .then(response => response.json())
+      .then(response => setNews(response.value))
+      .catch(err => console.error(err));
+   
   }, []);
-  console.log(news, "sachin news");
+ 
 
   return (
     <>
@@ -28,14 +39,14 @@ function NewsCard() {
         { news?.length>0 && 
          news
           ?.filter((item) => {
-            return item.title.toLowerCase().includes(input.toLowerCase());
+            return item.description.toLowerCase().includes(input.toLowerCase());
           })
           .map((item, index) => (
             <div key={index} className={style.container}>
-              <h6>{item?.author}</h6>
-              <img className={style.img} src={item.urlToImage} />
-              <h5>{item.title}</h5>
-              <h6>{item.publishedAt}</h6>
+              <h6>{item?.provider?.name}</h6>
+              <img className={style.img} src={item.image.url} />
+              <h5>{item.description}</h5>
+              <h6>{item.datePublished}</h6>
             </div>
           ))}
       </div>
