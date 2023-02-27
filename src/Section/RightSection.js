762ec0a0ad43632ba44968a5dcbbf1e111434e9e
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-02-27 15:08:23 +0530

    searchBox

--- src/Section/RightSection.js
+++ src/Section/RightSection.js
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from 'react'
 import style from "./RigthSection.module.css"
 import { getNews } from '../Api/NewsApi'
 import NewsCard from '../NewsCard/NewsCard'
+import NavBar from '../NavBar/NavBar'
 
 function RightSection() {
  
   return (
     <>
    <div className={style.container}>  
+  <div className={style.NavBar}><NavBar/></div> 
    <NewsCard />
    </div>
    </>
