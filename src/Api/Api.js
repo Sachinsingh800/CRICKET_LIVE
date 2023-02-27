5ca809416bec58ea9bd4cb645cef1c6b70728f78
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-02-27 17:34:05 +0530

    api key

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,17 +1,10 @@
-const  API_KEY="d0b88f9e-e812-4e89-a36f-b07bfb63f530"
+const  API_KEY="02bf54a5-e862-486b-a426-57db41004cef"
 
 export const getMatches=()=>{
 
-    const url=`https://api.cricapi.com/v1/currentMatches?apikey=d0b88f9e-e812-4e89-a36f-b07bfb63f530&offset=0?apikey=${ API_KEY}`
+    const url=`https://api.cricapi.com/v1/currentMatches?apikey=02bf54a5-e862-486b-a426-57db41004cef&offset=0?apikey=${ API_KEY}`
     return fetch(url)
     .then((response)=>response.json())
     .catch((error)=>console.log("Error :" , error))
 }
 
-export const getMatchInfo=()=>{
-
-    const url=`https://api.cricapi.com/v1/players_info?apikey=d0b88f9e-e812-4e89-a36f-b07bfb63f530&id=8d553533-7155-4744-b51a-80b53c19b353`
-    return fetch(url)
-    .then((response)=>response.json())
-    .catch((error)=>console.log("Error :" , error))
-}
