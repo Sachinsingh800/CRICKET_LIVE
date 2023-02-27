c14e208c86bc27bfcfb0342c76ad07e8ae55871e
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-02-27 21:27:27 +0530

    Key id

--- src/NewsCard/NewsCard.js
+++ src/NewsCard/NewsCard.js
@@ -7,11 +7,6 @@ import { NewsApi } from "../Api/NewsApi";
 function NewsCard() {
   const [input, setInput] = useState("");
   const [news, setNews] = useState(NewsApi);
- console.log(news)
-
-
-
- 
 
   return (
     <>
@@ -23,19 +18,22 @@ function NewsCard() {
       />
 
       <div className={style.main}>
-        { news?.length>0 && 
-         news
-          ?.filter((item) => {
-            return item?.title.toLowerCase().includes(input.toLowerCase());
-          })
-          .map((item, index) => (
-            <div key={index} className={style.container}>
-              <h6>{item?.author}</h6>
-              <img className={style.img} src={item.image} />
-             <div className={style.title}> <h5>{item.title}</h5></div>
-              <h6>{item.publishedAt}</h6>
-            </div>
-          ))}
+        {news?.length > 0 &&
+          news
+            ?.filter((item) => {
+              return item?.title.toLowerCase().includes(input.toLowerCase());
+            })
+            .map((item, index) => (
+              <div key={index} className={style.container}>
+                <h6>{item?.author}</h6>
+                <img className={style.img} src={item.image} />
+                <div className={style.title}>
+                  {" "}
+                  <h5>{item.title}</h5>
+                </div>
+                <h6>{item.publishedAt}</h6>
+              </div>
+            ))}
       </div>
     </>
   );
--- src/Podcast/Podcast.js
+++ src/Podcast/Podcast.js
@@ -1,32 +1,41 @@
-
-import React from 'react'
-import NavBar from '../NavBar/NavBar'
-import style from "./Podcast.module.css"
-import CarouselSlide from '../CarousalSlide/CarouselSlide'
-import Footer from '../Footer/Footer'
-import { PodcastData } from '../PodcastData/PodcastData'
+import React from "react";
+import NavBar from "../NavBar/NavBar";
+import style from "./Podcast.module.css";
+import CarouselSlide from "../CarousalSlide/CarouselSlide";
+import Footer from "../Footer/Footer";
+import { PodcastData } from "../PodcastData/PodcastData";
 
 function Podcast() {
   return (
     <div className={style.container}>
-      <div className={style.NavBar}><NavBar/></div> 
+      <div className={style.NavBar}>
+        <NavBar />
+      </div>
 
       <div className={style.CarouselSlide}>
-      <CarouselSlide />
-      </div> 
-        <h1 className={style.header}>Podcast</h1>
-       
-     <div className={style.PodcastBox}>
-           {PodcastData.map((item)=>
-            <div className={style.podcast}>
-              <iframe width="300" height="230" src={`https://www.youtube.com/embed${item.url}`}title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-           </div>
-           )}
-    </div>  
-     
+        <CarouselSlide />
+      </div>
+      <h1 className={style.header}>Podcast</h1>
+
+      <div className={style.PodcastBox}>
+        {PodcastData.map((item, index) => (
+          <div key={index} className={style.podcast}>
+            <iframe
+              width="300"
+              height="230"
+              src={`https://www.youtube.com/embed${item.url}`}
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
+            ></iframe>
+          </div>
+        ))}
+      </div>
+
       <Footer />
     </div>
-  )
+  );
 }
 
-export default Podcast
+export default Podcast;
