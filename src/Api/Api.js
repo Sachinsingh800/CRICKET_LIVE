857e41f2d6f29d1dfc17edb617fd8290b626898e
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-02-27 20:46:48 +0530

    api

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,8 +1,8 @@
-const  API_KEY="02bf54a5-e862-486b-a426-57db41004cef"
+const  API_KEY="4720d6cd-e8d3-4ece-97bd-ee1a1fb269aa"
 
 export const getMatches=()=>{
 
-    const url=`https://api.cricapi.com/v1/currentMatches?apikey=02bf54a5-e862-486b-a426-57db41004cef&offset=0?apikey=${ API_KEY}`
+    const url=`https://api.cricapi.com/v1/currentMatches?apikey=4720d6cd-e8d3-4ece-97bd-ee1a1fb269aa&offset=0?apikey=${ API_KEY}`
     return fetch(url)
     .then((response)=>response.json())
     .catch((error)=>console.log("Error :" , error))
